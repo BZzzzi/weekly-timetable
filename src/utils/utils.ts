48fd3059507bb32a635a24b02862f73d34de7def
mnmhbbb <mnmhb64@gmail.com>
2024-12-09 00:06:46 +0900

    feat: 날짜를 넘기는 한국 기준 요일을 반환하는 유틸리티 함수 추가

---
+++ src/utils/utils.ts
@@ -0,0 +1,10 @@
+import dayjs from "dayjs";
+
+/**
+ * 날짜를 넘기면 한국 기준 요일을 반환하는 함수
+ * @param date 'YYYY-MM-DD' ex. '2024-12-09'
+ * @returns '월'
+ */
+export const shortDayOfWeek = (date: string) => {
+  return dayjs(date).locale("ko").format("ddd");
+};
